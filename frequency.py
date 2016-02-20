3cb6e0811ea39e8052982a14bbf81ffd2fab6668
Ian Neal <wopr@wopr.tech>
2016-02-20 14:15:32 -0700

    To have something to pull...

---
+++ frequency.py
@@ -0,0 +1,8 @@
+# Author: Ian Neal <wopr@wopr.tech>
+
+def find_frequency(plaintext):
+    """
+    Given a plaintext, find the frequency of each monograph, bigraph,
+    trigraph, and pentagraph
+    """
+    
